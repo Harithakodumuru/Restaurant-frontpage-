f0508d6bbbe3ffc87c6d45330d37270e3813e602
Haritha K <128407092+Harithakodumuru@users.noreply.github.com>
2023-07-24 10:55:46 +0530

    Add files via upload

---
+++ Restarunt website/javascript.js
@@ -0,0 +1,14 @@
+let food1s = document.getElementById('food1');
+let food2s = document.getElementById('food2');
+let foods = document.getElementById('food');
+
+
+food1s.addEventListener('click',()=>{
+    foods.style.backgroundImage = " url('res1.jpg')";
+
+})
+
+food2s.addEventListener('click',()=>{
+    foods.style.backgroundImage = "url('images\ 2.jpg')";
+    
+})
